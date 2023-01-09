11f9a450998be7946a31e2c0b80c37dcd91bbdc8
collet-david <collet.david@protonmail.com>
2023-01-09 13:11:49 +0100

    Maj

--- Scripts/JDR Aymeric/script.js
+++ Scripts/JDR Aymeric/script.js
@@ -14,13 +14,16 @@ var indexAleatoireL5 = Math.floor(Math.random() * ligne5.length);
 var resultatTheme = themes[indexAleatoireTheme];
 var resultat = ligne1[indexAleatoireL1] + "<br>" + ligne2[indexAleatoireL2] + "<br>" + ligne3[indexAleatoireL3]+ "<br>" + ligne4[indexAleatoireL4]+ "<br>" + ligne5[indexAleatoireL5];
 
-    
+  
+
       $('#resultat').html(resultat);
       $('#resultatTheme').html(resultatTheme);
 
     }
   
-    
+   
+
+
 
 
 
